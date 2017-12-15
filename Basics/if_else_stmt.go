bc1c8996a5b9c6381cce9a7e2c14f2aadf3b6747
Akash Jeez <akashit63@gmail.com>
2017-12-15 18:48:26 +0530

    if..else.. statement in GO!

---
+++ Basics/if_else_stmt.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	a := 100
+	if( a < 20 ) {
+		fmt.Println(" A is less than 20.")
+	} else {
+		fmt.Println(" A is greater than 20.")
+	}
+	fmt.Println(" Value of A :", a)
+}
