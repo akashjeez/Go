b8165a9cf619b5c0bcac9c765dbc8a9aa2a8c87c
Akash Jeez <akashit63@gmail.com>
2017-12-15 18:25:39 +0530

    Bitwise Operators

---
+++ Basics/Bitwise.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	a, b := 60, 13
+	fmt.Println(" Value of A and B :",a, b)
+	fmt.Println(" Bitwise operator & for A and B :",a&b)
+	fmt.Println(" Bitwise operator | for A and B :",a|b)
+	fmt.Println(" Bitwise operator ^ for A and B :",a^b)
+	fmt.Println(" Bitwise operator << for A by 2 :",a<<2)
+	fmt.Println(" Bitwise operator >> for A by 2 :",a>>2)
+}
