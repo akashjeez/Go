0d4e86614d6f6e21498f60225373c9cced85021c
Akash Jeez <akashit63@gmail.com>
2017-12-15 18:51:09 +0530

    Nested If statement

---
+++ Basics/Nested_if_stmt.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	a, b := 100, 200
+	if( a == 100 ) {
+		if( b == 200 ) {
+			fmt.Printf(" Value of A is 100 and B is 200.s")
+		}
+	}
+	fmt.Println(" Exact Value of A and B :", a, b)
+}
