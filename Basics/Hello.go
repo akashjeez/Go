ad3b04877b49522e079308e6411e265076cf9b1f
akashjeez <akashit63@gmail.com>
2017-12-15 16:00:25 +0530

    Simple Hi Message in GO!

---
+++ Basics/Hello.go
@@ -0,0 +1,7 @@
+//Download GO Compiler using below link
+//https://redirector.gvt1.com/edgedl/go/go1.9.2.windows-amd64.msi
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hi there, Welcome to Go Programming Language!!");
+}
