e519a6228d8a2643b861e2e27310c65fe9bb0af4
Akash Jeez <akashit63@gmail.com>
2017-12-15 18:46:27 +0530

    if statement in GO!

---
+++ Basics/if_stmt.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+	/* local variable definition */
+	a := 10
+	/* check the boolean condition using if statement */
+	if( a < 20 ) {
+		/* if condition is true then print the following */
+		fmt.Println(" A is less than 20.")
+	}
+	fmt.Println(" Value of A :", a)
+}
